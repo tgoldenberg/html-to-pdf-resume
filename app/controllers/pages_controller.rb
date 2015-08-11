0d1533006923335c7f6ae29717488bfcc1556331
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-10 21:43:22 -0400

    modify resume format

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,10 @@ class PagesController < ApplicationController
       format.pdf do
         render :pdf => 'file_name',
                :template => 'pages/home.pdf.erb',
-               :layout => 'pdf.html'
+               :layout => 'pdf.html',
+               :disable_external_links => false,
+               :enable_external_links => true,
+               :disable_internal_links => true
       end
     end
   end
